9b563ed63d91b18174923c6a975fc4dd5e0804da
pillowtrucker <githubino@janitor.chat>
2024-06-06 17:12:18 +0200

    update build instructions

--- build.zsh
+++ build.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-cmake -GNinja -S . -B build -DCMAKE_BUILD_TYPE=Debug -DTHERAC_SIM_WARNING_AS_ERROR=OFF
+cmake -GNinja -S . -B build -DCMAKE_BUILD_TYPE=Debug -DTHERAC_SIM_WARNING_AS_ERROR=OFF -DFLOAT_PRECISION=double -DCMAKE_INSTALL_PREFIX=install-test
 cmake --build build --parallel 8
